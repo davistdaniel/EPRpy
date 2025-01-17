dd8f5fdaf064786419be8d6ad4e15cb9afd8c437
d.daniel <d.daniel@fz-juelich.de>
2025-01-17 17:47:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,37 @@
-from setuptools import setup,find_packages # type: ignore
+from setuptools import setup, find_packages  # type: ignore
 
 setup(
     name='eprpy',
-    version='0.9.0',
-    description="A python library for working with EPR spectroscopic DATA.",
-    python_requires=">=3.9, <3.13.1",
-    author_email="davisthomasdaniel@gmail.com",
-    install_requires=[
-        'numpy>1.25.1,<=1.26.4',
-        'matplotlib'
+    version='0.9.0a2',
+    description="A Python library for working with EPR spectroscopic data.",
+    long_description=open("README.md").read(),  
+    long_description_content_type="text/markdown",  
+    python_requires=">=3.9, <3.13.1",  
+    author="Davis Thomas Daniel",  
+    author_email="davisthomasdaniel@gmail.com",  
+    url="https://github.com/davistdaniel/EPRpy",  
+    classifiers=[
+        "Development Status :: 3 - Alpha",  
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Topic :: Scientific/Engineering :: Chemistry",
+        "License :: OSI Approved :: MIT License",  
     ],
-    packages=find_packages()
+    install_requires=[
+        'numpy>=1.25.1,<=1.26.4',
+        'matplotlib',
+        'tqdm'
+    ],
+    packages=find_packages(),
+    project_urls={
+        "Bug Tracker": "https://github.com/davistdaniel/EPRpy/issues",
+        "Documentation": "https://davistdaniel.github.io/EPRpy/",
+        "Source Code": "https://github.com/davistdaniel/EPRpy",
+    },
 )
